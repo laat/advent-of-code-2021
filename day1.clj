302b67be6e3e546eb76d20f2c96bdb7b5aaaada0
Sigurd Fosseng <sigurd@fosseng.net>
2021-12-01 20:13:34 +0100

    day1

--- day1.clj
+++ day1.clj
@@ -1,12 +1,9 @@
 (ns laat.aoc2021.day1
-  (:require
-   [aocd.core :as aoc]
-   [clojure.string :refer [split-lines]])
+  (:require [clojure.string :refer [split-lines]])
   (:gen-class))
 
-(def input (aoc/input 2021 1))
-
-(->> input
+(->> "day1.txt"
+     slurp
      split-lines
      (map bigint)
      (partition 2 1)
@@ -14,7 +11,8 @@
      count)
 ;; => 1387
 
-(->> input
+(->> "day1.txt"
+     slurp
      split-lines
      (map bigint)
      (partition 3 1)
